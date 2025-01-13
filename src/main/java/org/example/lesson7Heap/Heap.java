9c40ab2256d866daccebe5dad75a05533500d695
Egor Pozdnov <pozdner98@gmail.com>
2025-01-13 22:50:13 +0300

    Fix lesson 7

--- src/main/java/org/example/lesson7Heap/Heap.java
+++ src/main/java/org/example/lesson7Heap/Heap.java
@@ -13,13 +13,10 @@ class Heap
     {
         int tree_size = (int)Math.pow(2, (depth + 1)) - 1;
         HeapArray = new int[tree_size];
-        Arrays.sort(a);
 
-        for (int i = a.length - 1, x = 0; i >= 0 && x < tree_size; i--, x++) {
-            HeapArray[x] = a[i];
+        for (int key : a) {
+            Add(key);
         }
-
-        lastInd = a.length;
     }
 
     public int GetMax()
@@ -32,43 +29,41 @@ class Heap
         int lastExistInd = lastInd - 1;
 
         if (lastExistInd == 0) {
+            HeapArray[0] = 0;
+            lastInd--;
             return root;
         }
 
         HeapArray[0] = HeapArray[lastExistInd];
         HeapArray[lastExistInd] = 0;
 
-        findNewRoot(0);
+        lastInd--;
+
+        compareAndSwapRootWithChild(0);
 
         return root;
     }
 
-    private void findNewRoot(int index) {
-       if (index >= lastInd || isBiggerThanChild(index) || 2 * index + 1 >= HeapArray.length) {
+    private void compareAndSwapRootWithChild(int index) {
+       if (index >= lastInd || 2 * index + 1 > lastInd - 1) {
            return;
        }
 
-       final int newIndex;
+       int biggerChildIndex = HeapArray[2 * index + 1] >= HeapArray[2 * index + 2] || 2 * index + 2 >= lastInd
+               ? 2 * index + 1
+               : 2 * index + 2;
 
-       if (HeapArray[2 * index + 1] > HeapArray[index]) {
-           int child = HeapArray[2 * index + 1];
-           HeapArray[2 * index + 1] = HeapArray[index];
-           HeapArray[index] = child;
-           newIndex = 2 * index + 1;
+       int newIndex;
+       if (HeapArray[biggerChildIndex] > HeapArray[index]) {
+           int key = HeapArray[index];
+           HeapArray[index] = HeapArray[biggerChildIndex];
+           HeapArray[biggerChildIndex] = key;
+           newIndex = biggerChildIndex;
        } else {
-           int child = HeapArray[2 * index + 2];
-           HeapArray[2 * index + 2] = HeapArray[index];
-           HeapArray[index] = child;
-           newIndex = 2 * index + 2;
+           return;
        }
 
-        findNewRoot(newIndex);
-    }
-
-    private boolean isBiggerThanChild(int index) {
-        return 2 * index + 1 < HeapArray.length
-                && HeapArray[2 * index + 1] < HeapArray[index]
-                && HeapArray[2 * index + 2] < HeapArray[index];
+        compareAndSwapRootWithChild(newIndex);
     }
 
     public boolean Add(int key)
@@ -87,7 +82,7 @@ class Heap
     }
 
     private void replaceNewKeyToRootIfNeeded(int keyInd, int parentInd) {
-        if (parentInd < 0 || HeapArray[parentInd] >= HeapArray[keyInd]) {
+        if (parentInd == keyInd || HeapArray[parentInd] >= HeapArray[keyInd]) {
             return;
         }
 
--- src/test/java/org/example/lesson7Heap/HeapTest.java
+++ src/test/java/org/example/lesson7Heap/HeapTest.java
@@ -16,6 +16,30 @@ public class HeapTest {
     }
 
     @Test
+    void GetMaxEmptyTest() {
+        Heap heap = new Heap();
+
+        heap.MakeHeap(new int[2], 2);
+
+        heap.GetMax();
+        heap.GetMax();
+        int res = heap.GetMax();
+
+        assertThat(res).isEqualTo(-1);
+    }
+
+    @Test
+    void AddEmptyTest() {
+        Heap heap = new Heap();
+
+        heap.MakeHeap(new int[5], 2);
+
+        boolean res = heap.Add(15);
+
+        assertThat(res).isTrue();
+    }
+
+    @Test
     void AddTest() {
         Heap heap = new Heap();
 
@@ -24,5 +48,25 @@ public class HeapTest {
         boolean res = heap.Add(15);
 
         assertThat(res).isFalse();
+    }
+
+    @Test
+    void GetAllTest() {
+        Heap heap = new Heap();
+
+        heap.MakeHeap(new int[]{1,2,3,4,5,6,7}, 2);
+
+        heap.GetMax();
+        heap.GetMax();
+        heap.GetMax();
+        heap.GetMax();
+        heap.GetMax();
+        heap.GetMax();
+        int last = heap.GetMax();
+
+        int res = heap.GetMax();
+
+        assertThat(res).isEqualTo(-1);
+        assertThat(last).isEqualTo(1);
     }
 }
